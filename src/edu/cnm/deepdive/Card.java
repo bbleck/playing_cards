b0237f92deb2cb49e7e14d6b4352e9d3facf85a8
brianb <bbleck@unm.edu>
2018-10-08 13:58:37 -0600

    Added Hash and Compare, as well as a Comparator class.

--- src/edu/cnm/deepdive/Card.java
+++ src/edu/cnm/deepdive/Card.java
@@ -3,8 +3,9 @@ package edu.cnm.deepdive;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
-public class Card {
+public class Card implements Comparable<Card>{
 
   private Rank rank;
   private Suit suit;
@@ -53,5 +54,33 @@ public class Card {
 
   public Suit getSuit() {
     return suit;
+  }
+
+  @Override
+  public int compareTo(Card other) { //method for this class to compare itself to other
+    int comparison = this.suit.compareTo(other.suit);
+    if ( comparison == 0) {
+      return this.rank.compareTo(other.rank);
+    } else {
+      return comparison;
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Card card = (Card) o;
+    return rank == card.rank &&
+        suit == card.suit;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(rank, suit);
   }
 }
